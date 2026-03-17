cd7a1529e27b2e69e27546b1f7b84e4ed0c933d0
nkasenides <hfnovember@hotmail.com>
2026-03-18 01:07:19 +0200

    refactor Leaderboard model to use Map for players and add PointType generic

--- lib/src/core/model/leaderboard/leaderboard.dart
+++ lib/src/core/model/leaderboard/leaderboard.dart
@@ -3,18 +3,35 @@ import 'package:athlos/src/core/model/leaderboard/leaderboard_scope.dart';
 import 'package:athlos/src/core/model/player.dart';
 
 /// Represents a leaderboard.
-abstract class Leaderboard<PlayerType extends Player> {
+abstract class Leaderboard<PlayerType extends Player, PointType extends num> {
+  // The name of the leaderboard, which can be used for display and retrieval.
+  String name;
 
-  String name; // The name of the leaderboard, which can be used for display and retrieval.
-  String worldSessionID; // The ID of the world session associated with the leaderboard.
-  String description; // A description of the leaderboard, which can be used for display and retrieval.
-  LeaderboardOrder order; // The order in which players are ranked on the leaderboard, which can be used for display and retrieval.
-  LeaderboardScope scope; // The scope of the leaderboard, which can be used for display and retrieval.
-  List<PlayerType> players = []; // A list of players on the leaderboard, which can be used for display and retrieval.
-  int createdOn = DateTime.now().millisecondsSinceEpoch;  // The timestamp of when the leaderboard was created, which can be used for display and retrieval.
+  // The ID of the world session associated with the leaderboard.
+  String worldSessionID;
 
-  Leaderboard(this.name, this.worldSessionID, this.description, this.order,
-      this.scope, this.players,
-      this.createdOn);
+  // A description of the leaderboard, which can be used for display and retrieval.
+  String description;
 
+  // The order in which players are ranked on the leaderboard, which can be used for display and retrieval.
+  LeaderboardOrder order;
+
+  // The scope of the leaderboard, which can be used for display and retrieval.
+  LeaderboardScope scope;
+
+  // A map of players to points representing the leaderboard, which can be used for display and retrieval.
+  Map<PlayerType, PointType> players = {};
+
+  // The timestamp of when the leaderboard was created, which can be used for display and retrieval.
+  int createdOn = DateTime.now().millisecondsSinceEpoch;
+
+  Leaderboard({
+    required this.name,
+    required this.worldSessionID,
+    required this.description,
+    required this.order,
+    required this.scope,
+    required this.players,
+    required this.createdOn,
+  });
 }
